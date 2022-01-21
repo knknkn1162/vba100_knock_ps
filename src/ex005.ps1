c1e694b906e7eae6af241e1765cfa4735aaca5b3
knknkn1162 <knknkn1162@gmail.com>
2022-01-21 15:20:06 +0900

    implement ps in ex005.xlsm

---
+++ src/ex005.ps1
@@ -0,0 +1,9 @@
+function Run-Macro($app, $book) {
+    $rng = $book.Worksheets(1).Range("B2").CurrentRegion
+    $rng = $app.Intersect($rng, $rng.Offset(1))
+    $rng.Columns(3).NumberFormatLocal = "\#,##0"
+    $rng.Columns(1).Cells | `
+        ? {[String]$_.Value() -ne ""} | `
+        ? {[String]$_.Offset(0,1).Value() -ne ""} | `
+        % {$_.Offset(0,2).FormulaR1C1 =  "=RC[-2] * RC[-1]"}
+}
