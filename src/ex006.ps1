51d1123ff9ee26f6dddaadfa8cae5c3189aaa4eb
knknkn1162 <knknkn1162@gmail.com>
2022-01-21 15:48:03 +0900

    implement ps in ex006.xlsm

--- src/ex006.ps1
+++ src/ex006.ps1
@@ -1,6 +1,7 @@
 function Run-Macro($app, $book) {
     $rng = $book.Worksheets(1).Range("A1").CurrentRegion
     $rng = $app.Intersect($rng, $rng.Offset(1))
+    $rng.Columns(4).NumberFormatLocal = "\#,##0"
     $rng.Columns(1).Cells | `
         ? {[String]$_.Value() -notlike "*-*"} | `
         % {$_.Offset(0,3).FormulaR1C1 =  "=RC[-2] * RC[-1]"}
