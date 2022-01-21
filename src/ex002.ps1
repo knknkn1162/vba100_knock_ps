e0d22f0b64486cfcf0f84012a9d8578a64268828
knknkn1162 <knknkn1162@gmail.com>
2022-01-21 11:33:18 +0900

    fix args ps in ex002.xlsm

--- src/ex002.ps1
+++ src/ex002.ps1
@@ -1,7 +1,8 @@
-function Run-Macro($book) {
+function Run-Macro($app, $book) {
     $ws2 = $book.Worksheets("Sheet2")
     $book.Worksheets("Sheet1").Range("A1:C5").Copy()
     @($XlPasteType::xlPasteValues, $XlPasteType::xlPasteFormats) | `
         % { $ws2.Range("A1").PasteSpecial($_) }
-    $book.Parent.CutCopyMode = $false
+    # TODO: value__ is necessary because $app.cutCopyMode permits only [Microsoft.Office.Interop.Excel.XlCutCopyMode] but $true/$false is also OK
+    $app.cutCopyMode.__value = $false
 }
