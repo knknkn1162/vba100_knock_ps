7f25d95c99d8a6d1bef06178aca73240865a8957
knknkn1162 <knknkn1162@gmail.com>
2022-01-22 15:38:53 +0900

    implement ps in ex012.xlsm

--- src/ex012.ps1
+++ src/ex012.ps1
@@ -1,12 +1,12 @@
 function Run-Macro($app, $book) {
-    $rng = $book.Worksheets(1).Range("A1").CurrentRegion.Offset(1).Columns(3).Cells |`
+    $book.Worksheets(1).Range("A1").CurrentRegion.Offset(1).Columns(3).Cells |`
         ? {$_.MergeCells()} |`
-        ? {$_.MergeArea(1).Address() -eq $_.Address()}
-    foreach($r in $rng) {
-        $rng2 = $r.MergeArea()
-        $val = $r.Value(); $cnt = $rng2.Count
-        $rng2.Unmerge()
-        $rng2.Value() = [Math]::Truncate($val/$cnt)
-        $rng2.Resize($val % $cnt) | %{$_.Value() += 1}
-    }
+        ? {$_.MergeArea(1).Address() -eq $_.Address()} | `
+        % {
+            $rng2 = $_.MergeArea(); $rng2.Unmerge()
+            $val = $_.Value(); $cnt = $rng2.Count
+
+            $rng2.Value() = [Math]::Truncate($val/$cnt)
+            $rng2.Resize($val % $cnt) | %{$_.Value() += 1}
+        }
 }
