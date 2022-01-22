ba0ad3b15a24960e70a755b486dcae54c94595af
knknkn1162 <knknkn1162@gmail.com>
2022-01-22 22:42:58 +0900

    implement ps in ex001.xlsm

--- src/ex001.ps1
+++ src/ex001.ps1
@@ -1,3 +1,3 @@
 function Run-Macro($app, $book) {
-    $book.Worksheets("Sheet1").Range("A1:C5").Copy($book.Worksheets("Sheet2").Range("A1"))
+    [void]$book.Worksheets("Sheet1").Range("A1:C5").Copy($book.Worksheets("Sheet2").Range("A1"))
 }
