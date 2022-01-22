8d57a98abd166c79c88530cd70a999f1fe87b49e
knknkn1162 <knknkn1162@gmail.com>
2022-01-22 22:44:50 +0900

    implement ps in ex004.xlsm

--- src/ex004.ps1
+++ src/ex004.ps1
@@ -1,4 +1,6 @@
 function Run-Macro($app, $book) {
     $rng = $book.Worksheets(1).Range("A1").CurrentRegion
-    $app.Intersect($rng.Offset(1,1), $rng).SpecialCells($XlCellType::xlCellTypeConstants).ClearContents()
+    $app.Intersect($rng.Offset(1,1), $rng) `
+        .SpecialCells($XlCellType::xlCellTypeConstants) `
+        .ClearContents()
 }
