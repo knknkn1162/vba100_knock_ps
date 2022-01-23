d4d1f9443976a5ed91b620aecac380c732b392e0
knknkn1162 <knknkn1162@gmail.com>
2022-01-23 22:19:21 +0900

    implement ps in ex017.xlsm

---
+++ src/ex017.ps1
@@ -0,0 +1,19 @@
+
+# TODO: "Unable to cast object of type 'System.Object[]' to type 'System.Object[,]'" in usual way, so we use transpose VBA function twice to cast.(But this is too sticky:(
+function cast2d($app, $arr2) {
+    return ,($app.WorkSheetFunction.transpose($app.WorkSheetFunction.transpose($arr2)))
+}
+function Run-Macro($app, $book) {
+    $ws = $book.Worksheets("部・課マスタ")
+    $rng = $book.Worksheets("社員").Range("A1").CurrentRegion.Columns("C")
+    $rng = $app.Intersect($rng, $rng.Offset(1))
+    $arr = $rng.Cells |`
+        %{($_.Resize($xlnull,4).Value()) -join ","} |`
+        sort |`
+        unique |`
+        # avoid to flatten 1d-array
+        %{,($_ -split ",")}
+    Write-Info ("arr type: " + $arr.GetType())
+
+    $ws.Range("A2").Resize($arr.length,4) = cast2d $app $arr
+}
