215954d394720101b26edaac18e7a9cb4220ed75
knknkn1162 <knknkn1162@gmail.com>
2022-01-24 10:33:03 +0900

    implement ps in ex020.xlsm

---
+++ src/ex020.ps1
@@ -0,0 +1,6 @@
+function Run-Macro($app, $book) {
+    $dir = $book.Path + "/BACKUP"
+    If ( !(Test-Path $dir) ) { mkdir $dir }
+    $fname = "ex020_{0}.xlsm" -f (Get-Date -f "yyyyMMddhhmm")
+    $book.SaveCopyAs(("{0}/{1}" -f $dir, $fname))
+}
