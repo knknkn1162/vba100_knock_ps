23e96c5f2a38327f2150ccdc6815f5a249732283
knknkn1162 <knknkn1162@gmail.com>
2022-01-24 11:17:50 +0900

    fix dir in ex020

--- src/ex020.ps1
+++ src/ex020.ps1
@@ -1,5 +1,5 @@
 function Run-Macro($app, $book) {
-    $dir = $book.Path + "/BACKUP"
+    $dir = $book.Path + "/ex020_BACKUP"
     If ( !(Test-Path $dir) ) { mkdir $dir }
     $fname = "ex020_{0}.xlsm" -f (Get-Date -f "yyyyMMddhhmm")
     $book.SaveCopyAs(("{0}/{1}" -f $dir, $fname))
