91c52abc1250b730ce2d354079d9544f1ea7ab02
knknkn1162 <knknkn1162@gmail.com>
2022-01-24 14:46:14 +0900

    implement ps in ex025.xlsm

---
+++ src/ex025.ps1
@@ -0,0 +1,20 @@
+# TODO: "Unable to cast object of type 'System.Object[]' to type 'System.Object[,]'" in usual way, so we use transpose VBA function twice to cast.(But this is too sticky:(
+function cast2d($app, $arr2) {
+    return ,($app.WorkSheetFunction.transpose($app.WorkSheetFunction.transpose($arr2)))
+}
+
+function Run-Macro($app, $book) {
+    $ws = $book.Worksheets("売上")
+    $rng = $ws.Range("A1").CurrentRegion
+    $mat = $app.Intersect($rng, $rng.Offset(1,2)) |`
+        %{,@(
+            $ws.Cells(
+                [Math]::Truncate($_.Row/2) * 2,1).Value(),
+                $ws.Cells($_.Row,2).Value(),
+                $ws.Cells(1,$_.Column).Value(),
+                $_.Value()
+        )}
+    $db_ws = $book.Worksheets("売上DB")
+    $book.Worksheets("売上DB").Range("A2").Resize($mat.length, 4) = cast2d $app $mat
+    $db_ws.UsedRange.EntireColumn.AutoFit()
+}
