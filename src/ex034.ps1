bd558e1c50c9ec78967efcf0433cef8e8a3cf6ad
knknkn1162 <knknkn1162@gmail.com>
2022-01-30 23:50:47 +0900

    implement ps in ex034.xlsm

--- src/ex034.ps1
+++ src/ex034.ps1
@@ -1,17 +1,22 @@
-function transpose([Object[][]]$arr, [boolean]$direction) {
-    #Write-Info $arr[1].Length
-    #Write-Info $arr[1,2].GetType()
-    #$arr |`
-    #    %{$tmp=$_; $_|%{$_}
-    #    }
+function transpose([Object[,]]$arr, [boolean]$direction) {
+    $rows = $arr.GetLength(0)
+    $cols = $arr.GetLength(1)
+    $ret = New-Object "Object[,]" $cols,$rows
+    foreach($i in 1..$cols) {
+        foreach($j in 1..$rows) {
+            [int]$r = $rows - $j + 1
+            [int]$c = $cols - $i + 1
+            $ret[[int]($i - 1),[int]($j - 1)] = if($direction) {$arr[$r, $i]} else {$arr[$j, $c]}
+        }
+    }
+    return ,$ret
 }
-function Run-Macro($app, $book) {
-    $arr = @(,@())
 
-    # 1-indexed
-    $book.Worksheets(1).Range("A1").CurrentRegion.Cells |`
-        %{$arr += ,$_}
-    Write-Info $arr[2].Length
-    Write-Info $arr.Length
-    #transpose $book.Worksheets(1).Range("A1").CurrentRegion.Cells 0
+function Run-Macro($app, $book) {
+    $ws = $book.Worksheets(1)
+    $rng = $ws.Range("A1").CurrentRegion
+    # 1-indexed(Object[,])
+    $arr2 = $rng.Value()
+    $ret = transpose $arr2 $false
+    $ws.Range("F1").Resize($rng.Columns.Count, $rng.Rows.Count) = $ret
 }
