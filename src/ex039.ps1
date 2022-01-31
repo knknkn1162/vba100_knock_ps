f69e5a06d1a806eca05c123a58d8954ac83c5f1b
knknkn1162 <knknkn1162@gmail.com>
2022-01-31 22:25:42 +0900

    implement ps in ex039.xlsm

--- src/ex039.ps1
+++ src/ex039.ps1
@@ -2,6 +2,8 @@ function Run-Macro($app, $book) {
     $rng = $book.Worksheets(1).Range("A1").CurrentRegion
     $rngA = $rng.Columns(1)
     $rngB = $rng.Columns(2)
+    $ddd = $rngA.Cells | %{$_.Value}
+    ,$ddd | gm
     [int[]]$arr = ($rngA.Value() + $rngB.Value()) | sort | unique
     $rng.Columns(3).Resize($arr.Length, 1) = $app.WorksheetFunction.transpose($arr)
 }
