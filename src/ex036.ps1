90374a19b496d857afc0cbeb6da23e5520accb3d
knknkn1162 <knknkn1162@gmail.com>
2022-01-31 11:53:10 +0900

    implement ps in ex036.xlsm

--- src/ex036.ps1
+++ src/ex036.ps1
@@ -1,6 +1,14 @@
 function Run-Macro($app, $book) {
-    $rng = $book.Worksheets(1).Range("A1").CurrentRegion
-    $rng.Row(1).Cells |`
-        %{$_.Value() -match "\(([0-9]+)\)^"} |`
-        %{$Matches}
+    $ws = $book.Worksheets(1)
+    $rng = $ws.Range("A1").CurrentRegion
+    $arr = $rng.Rows(1).Cells |`
+        %{,@($_.Column, ($_.Value() -match "\(([0-9]+)\)$")) } |`
+        %{,@($_[0], $Matches[1])} |`
+        sort -Property {[int]$_[1]} |`
+        %{$_[0]}
+    $cnt = $arr.Length
+    1..$cnt |`
+        %{[void]$rng.Columns($arr[$_ - 1]).Copy($rng.Columns($cnt + $_))}
+    [void]$rng.Resize(1,$cnt).EntireColumn.Delete()
+    [void]$ws.UsedRange.EntireColumn.AutoFit()
 }
