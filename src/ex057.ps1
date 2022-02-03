0af256988ad5f3806bfb09531a75f65b304443d9
knknkn1162 <knknkn1162@gmail.com>
2022-02-03 21:56:08 +0900

    improve ex057.ps1

--- src/ex057.ps1
+++ src/ex057.ps1
@@ -4,20 +4,12 @@ function Date2Str($date) {
 function Run-Macro($app, $book) {
     $bakdir = "{0}/ex057_BACKUP" -f $book.Path
     $dic = @{}
-    ls $bakdir | %{
-        $datestr = Date2Str($_.LastWriteTime) 
-        $nval = @($_.LastWriteTime, $_.FullName)
-        if(!$dic.Contains($datestr)) {
-            $dic[$datestr] = $nval
-        } else {
-            # if $_ is old
-            if($dic[$datestr][0] -lt $_.LastWriteTime) {
-                $rmname = $dic[$datestr][1]
-                $dic[$datestr] = $nval
-            } else {
-                $rmname = $nval[1]
-            }
-            rm -r $rmname
-        }
-    }
+    ls $bakdir |`
+        sort -p LastWriteTime |`
+        %{$dic[(Date2Str $_.LastWriteTime)] = $_.FullName}
+    ls $bakdir |`
+        ?{!($dic[(Date2Str $_.LastWriteTime)] -eq $_.FullName)} |`
+        %{rm -r $_.FullName}
+
+
 }
