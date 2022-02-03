a04f8fdad827e4b64f691fba91edf944b10ee618
knknkn1162 <knknkn1162@gmail.com>
2022-02-03 16:58:11 +0900

    implement ps in ex061.xlsm

--- src/ex061.ps1
+++ src/ex061.ps1
@@ -1,12 +1,10 @@
 function Run-Macro($app, $book) {
     $mrng = $book.Worksheets("マスタ").Range("A1").CurrentRegion
     $dic = @{}
-    $dic = $app.Intersect($mrng, $mrng.Offset(1)).Columns(1).Cells |`
+    $app.Intersect($mrng, $mrng.Offset(1)).Columns(1).Cells |`
         %{$dic.Add($_.Value(), $_.Phonetic.Text)}
     $trng = $book.Worksheets("data").Range("A1").CurrentRegion
-    $dic
     $app.Intersect($trng, $trng.Offset(1)).Columns(1).Cells | %{
-        Write-Info $_.Address(), $_.Value()
         if($dic.containsKey($_.Value())) {
             $_.Phonetic.Text = $dic[$_.Value()]
         } else {
