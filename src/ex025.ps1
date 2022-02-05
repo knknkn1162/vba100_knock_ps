785b4c2bd39341ff5b341bfd8fc780c189362184
knknkn1162 <knknkn1162@gmail.com>
2022-02-05 23:44:06 +0900

    ex025.ps1

--- src/ex025.ps1
+++ src/ex025.ps1
@@ -7,11 +7,10 @@ function Run-Macro($app, $book) {
     $rng = $ws.Range("A1").CurrentRegion
     $mat = $app.Intersect($rng, $rng.Offset(1,2)) |`
         %{,@(
-            $ws.Cells(
-                [Math]::Truncate($_.Row/2) * 2,1).Value(),
-                $ws.Cells($_.Row,2).Value(),
-                $ws.Cells(1,$_.Column).Value(),
-                $_.Value()
+            $ws.Cells([Math]::Truncate($_.Row/2) * 2,1).Value(),
+            $ws.Cells($_.Row,2).Value(),
+            $ws.Cells(1,$_.Column).Value(),
+            $_.Value()
         )}
     $db_ws = $book.Worksheets("売上DB")
     $book.Worksheets("売上DB").Range("A2").Resize($mat.length, 4) = cast2d $app $mat
