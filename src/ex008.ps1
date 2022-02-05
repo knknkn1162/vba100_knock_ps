70ab3ef75d459d60fa93a9724171a5bc2fe0afb2
knknkn1162 <knknkn1162@gmail.com>
2022-02-05 22:54:48 +0900

    improve ex008

--- src/ex008.ps1
+++ src/ex008.ps1
@@ -1,9 +1,9 @@
 function Run-Macro($app, $book) {
     $rng = $book.Worksheets(1).Range("A1").CurrentRegion
     $cols = $rng.Columns.Count()
-    $app.Intersect($rng, $rng.Offset(1,1).Columns(1)).Cells | `
-        ? {($_.Resize($xlnull,$cols-1).Value() | measure -sum).Sum -ge 350} | `
-        ? {$app.WorksheetFunction.CountIf($_.Resize($xlnull,$cols-1), 50) -eq 0} | `
-        % {$_.Offset($xlnull,$cols-1) = "合格"}
-    
+    $cands = $app.Intersect($rng, $rng.Offset(1,1).Columns(1)).Cells | `
+        %{,$_.Resize(1,$cols-1)} |`
+        ?{($_.Value() | measure -sum).Sum -ge 350} |`
+        ?{($_.Value() | ?{$_ -lt 50}).Length -eq 0} |`
+        %{$_.Resize(1,1).Offset(0,$cols-1) = "合格"}
 }
