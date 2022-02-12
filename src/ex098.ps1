12094668bb1b378076cf6f23ade15f8c644eaa8f
knknkn1162 <knknkn1162@gmail.com>
2022-02-12 23:41:22 +0900

    refactor ps in ex098.xlsm

--- src/ex098.ps1
+++ src/ex098.ps1
@@ -1,21 +1,20 @@
-function Get-RegularCells($crng, $nrng) {
+function Get-RegularAddrs($crng, $nrng) {
     $cperson = @{}
-    $crng.Cells |`
-        %{ $cperson.Add($_.Value(), @($_.Row, $_.Column)) }
+    $crng.Cells | %{ $cperson.Add($_.Value(), @($_.Row, $_.Column)) }
 
     $dir4 = @(@(0,1), @(0, -1), @(1, 0), @(-1, 0))
     $neighdir = @{}
-    $intparm = @{PassThru = $true; ExcludeDifferent = $true; IncludeEqual = $true }
     $crng.Cells |`
         %{ $r=$_; $neighdir.Add($r.Value(), [string[]]@($dir4 | %{$r.Offset($_[0], $_[1]).Value()} ) )}
+    
+    $intsecparm = @{PassThru = $true; ExcludeDifferent = $true; IncludeEqual = $true }
     $ret = $nrng.Cells |`
         # Check Different row, column
-        ?{ @(Compare-Object $cperson[$_.Value()] @($_.Row, $_.Column) @intparm -Sync 0).length -eq 0} |`
-        ?{
+        ?{ @(Compare-Object $cperson[$_.Value()] @($_.Row, $_.Column) @intsecparm -Sync 0).length -eq 0} | ?{
             $r=$_
             [string[]]$nneigh = @($dir4 | %{ @($r.Offset($_[0], $_[1]).Value()) })
             # check different neighbors
-            (Compare-Object $neighdir[$r.Value()] $nneigh @intparm).length -eq 0
+            (Compare-Object $neighdir[$r.Value()] $nneigh @intsecparm).length -eq 0
         } |`
         %{$_.Address()}
     return ,$ret
@@ -27,10 +26,9 @@ function Run-Macro($app, $book) {
     $sz = 6
     $crng = $cws.Range("B5").Resize($sz,$sz)
     $nrng = $nws.Range("B5").Resize($sz,$sz)
-    $regcells = Get-RegularCells $crng $nrng
+    $regcells = Get-RegularAddrs $crng $nrng
     Write-Info $regcells.length
     # check irregular cells
-    $nrng.Cells |`
-        ?{$_.Address() -notin $regcells} |`
-        %{ $_.Interior.Color = $xlEnum.XlRgbColor::rgbYellow }
+    Compare-Object @($nrng.Cells | %{$_.Address()}) $regcells -PassThru |`
+        %{ $nws.Range($_).Interior.Color = $xlEnum.XlRgbColor::rgbYellow }
 }
